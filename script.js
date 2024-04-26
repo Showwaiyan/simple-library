382a949b1bb14593a86e3d273051a827c65569b2
Show Wai Yan <showwaiyan555@gmail.com>
2024-04-26 20:03:46 +0630

    Card Disapper bug fixed

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const cardSection = document.querySelector("section");
 const closeBtn = document.querySelector(".close-btn")
 
 function toggleCard(e) {
-    cardSection.classList.toggle("hidden");
-    console.log(e.target);
-    e.stopPropagation();
+    if (e.target == addedBtn ||
+        e.target == cardSection ||
+        e.target == closeBtn) {
+            cardSection.classList.toggle("hidden");
+            e.stopPropagation();
+        }
+
 }
 
 addedBtn.addEventListener('click',toggleCard);
 
 closeBtn.addEventListener('click',toggleCard);
 
-cardSection.addEventListener('click',toggleCard,{capture: true});
+cardSection.addEventListener('click',toggleCard);
